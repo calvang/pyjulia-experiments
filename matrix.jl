173775a43cb429ac74a57cc55661b0044cf5cdd1
calvang <calvin.huang5@gmail.com>
2020-08-18 22:35:20 -0400

    Update import method

--- matrix.jl
+++ matrix.jl
@@ -1,28 +1,10 @@
 using PyCall
 using LinearAlgebra
 
-py"""
-import csv
+pushfirst!(PyVector(pyimport("sys")."path"), "")
+pymatrix = pyimport("matrix")
 
-def read_csv(filename):
-    with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        csv_data = []
-        for line in csv_reader:
-            row_data = []
-            for el in line:
-                row_data.append(int(el))
-            csv_data.append(row_data)
-        return csv_data
-
-def write_csv(matrix, filename):
-    with open(filename, mode='w') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',')
-        for row in matrix:
-            csv_writer.writerow(row)
-"""
-
-matArray = py"read_csv"("matrix.csv")
+matArray = pymatrix.read_csv("matrix.csv")
 println("Input Matrix:")
 println(matArray)
 println("Determinant:")
@@ -32,5 +14,8 @@ println(tr(matArray))
 println("Square:")
 squaredArray = matArray * matArray
 println(squaredArray)
-println("Writing square to file...")
-py"write_csv"(squaredArray, "output.csv")
+destroyedArray = squaredArray * squaredArray * det(squaredArray)
+println("Assortment of various matrix operations:\n", destroyedArray)
+println("Wow! That was fast!")
+println("Writing to file...")
+pymatrix.write_csv(destroyedArray, "output.csv")
